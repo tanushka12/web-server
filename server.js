029d8c7ab705bddfdca20bda9cd28337fbfd8420
tanushka12 <tanured@gmail.com>
2018-01-03 13:47:09 +0530

    projects added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -49,12 +50,18 @@ app.get('/about', (req,res) => {
   });
 });
 
+app.get('/projects', (req,res) => {
+  res.render('project.hbs',{
+    pageTitle: 'Projects',
+  });
+});
+
 app.get('/bad', (req,res) => {
   res.send({
     errorMessage: 'Unable to handle request'
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
